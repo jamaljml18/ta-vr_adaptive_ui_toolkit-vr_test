63fc8987a6d5cd3c3b2ec6711eab61bc38bd3836
jamalmhmmd18 <jamalmhmmd18@gmail.com>
2025-12-17 21:43:11 +0700

    Update: mengganti sdk pico dari 3.3.3 menjadi 3.0.5 karena lebih stabil dan tidak ada masalah pada second camera

---
+++ Assets/Scripts/Scene1/DebugUIController.cs
@@ -0,0 +1,115 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DebugUIController : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField] private GameObject curved_ui_button_panel;
+    [SerializeField] private GameObject curved_ui_image_panel;
+    [SerializeField] private GameObject curved_ui_toggle_panel;
+    [SerializeField] private GameObject curved_ui_scrollview_panel;
+    [SerializeField] private GameObject curved_ui_slider_panel;
+    [SerializeField] private GameObject curved_ui_scrollbar_panel;
+    [SerializeField] private GameObject curved_ui_dropdown_panel;
+
+    [Header("VR Input")]
+    [SerializeField] private InputActionReference vrInput; // [ID] Input VR yang akan dipakai untuk mendeteksi klik/trigger dalam VR
+                                                           // [EN] VR Input used to detect click/trigger events inside VR
+
+    private int index = 1;
+
+    private void OnEnable()
+    {
+        // [ID] Aktifkan listener input VR ketika object aktif
+        // [EN] Enable VR input listener when this object becomes active
+        if (vrInput != null)
+            vrInput.action.started += OnVRPressed;
+
+    }
+
+    private void OnDisable()
+    {
+        // [ID] Lepaskan listener VR untuk mencegah memory leak
+        // [EN] Remove VR listener to avoid memory leaks
+        if (vrInput != null)
+            vrInput.action.started -= OnVRPressed;
+
+    }
+
+    private void Start()
+    {
+        curved_ui_button_panel.SetActive(true);
+        curved_ui_image_panel.SetActive(false);
+        curved_ui_toggle_panel.SetActive(false);
+        curved_ui_scrollview_panel.SetActive(false);
+        curved_ui_slider_panel.SetActive(false);
+        curved_ui_scrollbar_panel.SetActive(false);
+        curved_ui_dropdown_panel.SetActive(false);
+    }
+
+    private void OnVRPressed(InputAction.CallbackContext ctx)
+    {
+        // [ID] Dipanggil saat input VR utama (misalnya trigger) ditekan
+        // [EN] Called when the primary VR input (e.g., trigger) is pressed
+
+        Debug.Log("VR INPUT TRIGGERED! Find");
+
+        // [ID] Tampilkan UI melengkung
+        // [EN] Show the curved UI
+        ShowUI();
+    }
+
+    private void ShowUI()
+    {
+        index++;
+        if (index > 8) index = 1;
+
+        // Nonaktifkan semua panel dulu
+        curved_ui_button_panel.SetActive(false);
+        curved_ui_image_panel.SetActive(false);
+        curved_ui_toggle_panel.SetActive(false);
+        curved_ui_scrollview_panel.SetActive(false);
+        curved_ui_slider_panel.SetActive(false);
+        curved_ui_scrollbar_panel.SetActive(false);
+        curved_ui_dropdown_panel.SetActive(false);
+
+        switch (index)
+        {
+            case 1:
+                curved_ui_button_panel.SetActive(true);
+                Debug.Log("Show Button Panel");
+                break;
+
+            case 2:
+                curved_ui_image_panel.SetActive(true);
+                Debug.Log("Show Image Panel");
+                break;
+
+            case 3:
+                curved_ui_toggle_panel.SetActive(true);
+                Debug.Log("Show Toggle Panel");
+                break;
+
+            case 4:
+                curved_ui_scrollview_panel.SetActive(true);
+                Debug.Log("Show ScrollView Panel");
+                break;
+
+            case 5:
+                curved_ui_slider_panel.SetActive(true);
+                Debug.Log("Show Slider Panel");
+                break;
+
+            case 6:
+                curved_ui_scrollbar_panel.SetActive(true);
+                Debug.Log("Show Scrollbar Panel");
+                break;
+
+            case 7:
+                curved_ui_dropdown_panel.SetActive(true);
+                Debug.Log("Show Dropdown Panel");
+                break;
+        }
+    }
+
+}
